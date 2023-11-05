0cf503c6cb18af3d924c4c836eac8e10b971b499
Mateusz <mateusz4k@outlook.com>
2023-11-05 14:56:34 +0100

    fix: the issue was that the effect wasn't smooth on the first hover.

--- src/components/pill-effect/Navigation.tsx
+++ src/components/pill-effect/Navigation.tsx
@@ -1,5 +1,5 @@
 /** @format */
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
 
 export const Navigation = () => {
@@ -31,7 +31,7 @@ export const NavItem = ({ itemName }: { itemName: string }) => {
 				className='relative px-3 py-1.5 '
 				href={`#${itemName}`}>
 				{itemName}
-				{isHovered && <Pill />}
+				<AnimatePresence>{isHovered && <Pill />}</AnimatePresence>
 			</a>
 		</motion.li>
 	);
@@ -40,6 +40,10 @@ export const NavItem = ({ itemName }: { itemName: string }) => {
 export const Pill = () => {
 	return (
 		<motion.div
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+			transition={{ duration: 0.3 }}
 			layoutId='pillow'
 			className='absolute inset-0 bg-gradient-to-r from-rose-500 to-rose-500/25 -z-10'
 			// Prevent distortion when changing the item, instead (rounded-full)
--- src/components/underline-effect/Navigation.tsx
+++ src/components/underline-effect/Navigation.tsx
@@ -1,5 +1,5 @@
 /** @format */
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
 
 export const Navigation = () => {
@@ -31,7 +31,7 @@ const NavItem = ({ itemName }: { itemName: string }) => {
 				className='relative cursor-pointer py-1.5'
 				href={`#${itemName}`}>
 				{itemName}
-				{isHovered && <Underline />}
+				<AnimatePresence>{isHovered && <Underline />}</AnimatePresence>
 			</a>
 		</motion.li>
 	);
@@ -40,6 +40,10 @@ const NavItem = ({ itemName }: { itemName: string }) => {
 const Underline = () => {
 	return (
 		<motion.div
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+			transition={{ duration: 0.2 }}
 			layoutId='underline'
 			className='absolute -bottom-[1px] bg-gradient-to-r w-full from-rose-800 via-rose-300 to-rose-800 h-1'
 		/>
